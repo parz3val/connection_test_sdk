23111b86ccd8371767b3c00cebd4cc2cc8124319
Harris Kunwar <iamtheparzival@gmail.com>
2021-04-12 12:37:57 +0545

    Big channges to speed and ping test

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 // Entrypoint in the package
 const ipTest = require("./lib/network.js");
-const Connectivity = require("./lib/connectivity.js");
 const speedTest = require("./lib/speedTest.js");
-const proxy = require("./lib/proxy.js");
-const pingTest = require("./lib/pingTest.js");
 
 module.exports = {
   ipTest,
-  Connectivity,
   speedTest,
-  proxy,
-  pingTest,
 };
